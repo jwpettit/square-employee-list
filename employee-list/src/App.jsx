ce8bbb9acd5bc1fc085308b18cb521625c4e37fb
jwpettit <jwpettit@me.com>
2023-02-16 01:39:05 -0800

    Built fetch for JSON data

--- employee-list/src/App.jsx
+++ employee-list/src/App.jsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [employees, setEmployees] = useState([])
+  const [loadedEmployees, setLoadedEmployees] = useState(false)
+
+  useEffect(() => {
+    const getData = async () => {
+      let url = "https://s3.amazonaws.com/sq-mobile-interview/employees.json";
+      const response = await fetch(url);
+
+      if (response.ok) {
+        let data = await response.json();
+        setEmployees(data.employees);
+      } else {
+        console.error('Error searching for employees |', response)
+      }
+      setLoadedEmployees(true);
+    }
+
+    if (loadedEmployees === false) {
+      getData();
+    }
+  }, [employees, loadedEmployees]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +45,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
